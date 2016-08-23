0a67ede85be33728c4d18020afc8e28194305dee
Srdjan Perusinovic <s.perusinovic@levi9.com>
2016-08-23 16:08:48 +0200

    Added Select method to CodeAnalyzer class. Added new SymbolDispalyFormat for retrieving more descriptive method names.

--- CodeSamples/CodeAnalyzer.cs
+++ CodeSamples/CodeAnalyzer.cs
@@ -9,7 +9,7 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Emit;
 
-namespace CodeAnalyzer
+namespace CodeAnalysis
 {
     public class CodeAnalyzer
     {
@@ -19,7 +19,8 @@ namespace CodeAnalyzer
         private SemanticModel _semanticModel;
         private CompilationUnitSyntax _root;
         private SyntaxTree _syntaxTree;
-        private SymbolDisplayFormat _symbolDisplayFormat;
+        private SymbolDisplayFormat _variableSymbolDisplayFormat;
+        private SymbolDisplayFormat _methodSymbolDisplayFormt;
 
         /// <summary>
         /// Simple code analyzer.
@@ -27,7 +28,7 @@ namespace CodeAnalyzer
         /// to avoid creation of syntax tree and semantic model each time user wants list of used variable types or methods.
         /// </summary>
         /// <param name="code">Code that should be analyzed.</param>
-        public CodeAnalyzer (string code)
+        public CodeAnalyzer(string code)
         {
             if (string.IsNullOrEmpty(code))
             {
@@ -36,26 +37,45 @@ namespace CodeAnalyzer
             else
             {
                 _assemblyName = Path.GetRandomFileName();
-
                 _metadataReferences = new MetadataReference[]{
                     MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
                     MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location)
                 };
-
-                 _syntaxTree = CSharpSyntaxTree.ParseText(code);
-
+                _syntaxTree = CSharpSyntaxTree.ParseText(code);
                 _compilation = CSharpCompilation.Create(
                        _assemblyName,
                        syntaxTrees: new[] { _syntaxTree },
                        references: _metadataReferences,
                        options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
-
                 _semanticModel = _compilation.GetSemanticModel(_syntaxTree);
-
                 _root = (CompilationUnitSyntax)_syntaxTree.GetRoot();
 
-                _symbolDisplayFormat = new SymbolDisplayFormat(typeQualificationStyle: SymbolDisplayTypeQualificationStyle.NameAndContainingTypesAndNamespaces);
-                
+                // More information on SymbolDisplayFormat can be found at http://source.roslyn.io/#Microsoft.CodeAnalysis/SymbolDisplay/SymbolDisplayFormat.cs,9ba04f3b6d792331
+                _variableSymbolDisplayFormat = new SymbolDisplayFormat(typeQualificationStyle: SymbolDisplayTypeQualificationStyle.NameAndContainingTypesAndNamespaces);
+                _methodSymbolDisplayFormt = new SymbolDisplayFormat(
+                                                    globalNamespaceStyle: SymbolDisplayGlobalNamespaceStyle.OmittedAsContaining,
+                                                    typeQualificationStyle: SymbolDisplayTypeQualificationStyle.NameAndContainingTypesAndNamespaces,
+                                                    propertyStyle: SymbolDisplayPropertyStyle.ShowReadWriteDescriptor,
+                                                    localOptions: SymbolDisplayLocalOptions.IncludeType,
+                                                    genericsOptions: SymbolDisplayGenericsOptions.IncludeTypeParameters | SymbolDisplayGenericsOptions.IncludeVariance,
+                                                    memberOptions:
+                                                        SymbolDisplayMemberOptions.IncludeParameters |
+                                                        SymbolDisplayMemberOptions.IncludeContainingType |
+                                                        SymbolDisplayMemberOptions.IncludeType |
+                                                        SymbolDisplayMemberOptions.IncludeExplicitInterface,
+                                                    kindOptions:
+                                                        SymbolDisplayKindOptions.IncludeMemberKeyword,
+                                                    parameterOptions:
+                                                        SymbolDisplayParameterOptions.IncludeOptionalBrackets |
+                                                        SymbolDisplayParameterOptions.IncludeDefaultValue |
+                                                        SymbolDisplayParameterOptions.IncludeParamsRefOut |
+                                                        SymbolDisplayParameterOptions.IncludeExtensionThis |
+                                                        SymbolDisplayParameterOptions.IncludeType |
+                                                        SymbolDisplayParameterOptions.IncludeName,
+                                                    miscellaneousOptions:
+                                                        SymbolDisplayMiscellaneousOptions.EscapeKeywordIdentifiers |
+                                                        SymbolDisplayMiscellaneousOptions.UseErrorTypeSymbolName
+                );
             }
         }
 
@@ -74,7 +94,7 @@ namespace CodeAnalyzer
                 }
                 else
                 {
-                    result.Add(symbolInfo.Symbol.ToDisplayString(_symbolDisplayFormat));
+                    result.Add(symbolInfo.Symbol.ToDisplayString(_variableSymbolDisplayFormat));
                 }
             });
         }
@@ -86,14 +106,14 @@ namespace CodeAnalyzer
         /// <returns>List of method names.</returns>
         public List<string> GetMethodNames()
         {
-           return  SelectAndAnalyze<ExpressionSyntax>((result, syntaxExpression) => {
-               SymbolInfo symbolInfo = _semanticModel.GetSymbolInfo(syntaxExpression);
-      
-               if (symbolInfo.Symbol is IMethodSymbol && symbolInfo.Symbol.Name != ".ctor")
+            return SelectAndAnalyze<ExpressionSyntax>((result, syntaxExpression) => {
+                SymbolInfo symbolInfo = _semanticModel.GetSymbolInfo(syntaxExpression);
+
+                if (symbolInfo.Symbol is IMethodSymbol && symbolInfo.Symbol.Name != ".ctor")
                 {
-                    result.Add(symbolInfo.Symbol.ToDisplayString());
+                    result.Add(symbolInfo.Symbol.ToDisplayString(_methodSymbolDisplayFormt));
                 }
-           });
+            });
         }
 
         /// <summary>
@@ -102,9 +122,9 @@ namespace CodeAnalyzer
         /// <typeparam name="T">Type of nodes to select.</typeparam>
         /// <param name="action">Specific logic for extraction of methods and variable types.</param>
         /// <returns>List of method names or variable types.</returns>
-        private List<string> SelectAndAnalyze<T>(Action<List<string>, T> action) where T: CSharpSyntaxNode
+        private List<string> SelectAndAnalyze<T>(Action<List<string>, T> action) where T : CSharpSyntaxNode
         {
-            if (this._root == null && this._semanticModel == null)
+            if (this._root == null || this._semanticModel == null)
             {
                 throw new Exception("Value of _root or _semanticModel fields cannot be null.");
             }
@@ -112,16 +132,28 @@ namespace CodeAnalyzer
             List<T> syntaxSelection = _root.DescendantNodes().OfType<T>().ToList();
             List<string> result = new List<string>();
 
-            if(syntaxSelection.Count > 0)
+            if (syntaxSelection.Count > 0)
             {
-                foreach(var syntaxExpression in syntaxSelection)
+                foreach (var syntaxExpression in syntaxSelection)
                 {
                     SymbolInfo symbolInfo = _semanticModel.GetSymbolInfo(syntaxExpression);
                     action(result, syntaxExpression);
                 }
             }
-           
+
             return result;
+        }
+
+        /// <summary>
+        /// Utility method for counting accourances of specific syntax parts.
+        /// One example of usage is when you want to check how many types were defined in code,
+        /// you can pass TypeDeclarationSyntax as type parameter, and depending on result decide to execute that code or not.
+        /// </summary>
+        /// <typeparam name="T">Type parameter derived from CSharpSyntaxNode.</typeparam>
+        /// <returns>Count of accourances of specific syntax type in given code.</returns>
+        public int Count<T>() where T : CSharpSyntaxNode
+        {
+            return _root.DescendantNodes().OfType<T>().Count();
         }
     }
 }
