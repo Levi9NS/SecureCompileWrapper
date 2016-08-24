bd83560520a5dc320cca499affec1d303f3d838e
Srdjan Perusinovic <s.perusinovic@levi9.com>
2016-08-24 16:13:39 +0200

    Added additional functionalities to CodeAnalyzer.

--- CodeSamples/CodeAnalyzer.cs
+++ CodeSamples/CodeAnalyzer.cs
@@ -9,7 +9,7 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Emit;
 
-namespace CodeAnalysis
+namespace SecureCompileWrapper
 {
     public class CodeAnalyzer
     {
@@ -21,6 +21,7 @@ namespace CodeAnalysis
         private SyntaxTree _syntaxTree;
         private SymbolDisplayFormat _variableSymbolDisplayFormat;
         private SymbolDisplayFormat _methodSymbolDisplayFormt;
+        private Configuration _configuration;
 
         /// <summary>
         /// Simple code analyzer.
@@ -28,7 +29,7 @@ namespace CodeAnalysis
         /// to avoid creation of syntax tree and semantic model each time user wants list of used variable types or methods.
         /// </summary>
         /// <param name="code">Code that should be analyzed.</param>
-        public CodeAnalyzer(string code)
+        public CodeAnalyzer(string code, Configuration configuration = null)
         {
             if (string.IsNullOrEmpty(code))
             {
@@ -36,6 +37,7 @@ namespace CodeAnalysis
             }
             else
             {
+                _configuration = configuration;
                 _assemblyName = Path.GetRandomFileName();
                 _metadataReferences = new MetadataReference[]{
                     MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
@@ -84,7 +86,7 @@ namespace CodeAnalysis
         /// Nullable<T> is resolved as System.T?, for example Nullable<int> is resolved as System.Int32?. 
         /// </summary>
         /// <returns>List of all used types in format Namespace.Typename.</returns>
-        public List<string> GetVariableTypes()
+        public List<string> GetUsedVariableTypes()
         {
             return SelectAndAnalyze<VariableDeclarationSyntax>((result, syntaxExpression) => {
                 SymbolInfo symbolInfo = _semanticModel.GetSymbolInfo(syntaxExpression.Type);
@@ -100,11 +102,11 @@ namespace CodeAnalysis
         }
 
         /// <summary>
-        /// Creates list of method names used in code. Method name is in format Namespace.MethodName(Parameters).
+        /// Creates list of method names defined or invoked in code. Method name is in format "ReturnValue Namespace.MethodName(Parameters)".
         /// Constructors are ommited.
         /// </summary>
         /// <returns>List of method names.</returns>
-        public List<string> GetMethodNames()
+        public List<string> GetNamesForDefinedOrCalledMethods()
         {
             return SelectAndAnalyze<ExpressionSyntax>((result, syntaxExpression) => {
                 SymbolInfo symbolInfo = _semanticModel.GetSymbolInfo(syntaxExpression);
@@ -155,5 +157,109 @@ namespace CodeAnalysis
         {
             return _root.DescendantNodes().OfType<T>().Count();
         }
+
+        /// <summary>
+        /// Determines is given code safe to compile, according to provided configuration.
+        /// </summary>
+        /// <returns>Result of precompilation analysis in form of instance of PrecompilationAnalysisResult class.</returns>
+        public PrecompilationAnalysisResult PerformPrecompilationAnalysis()
+        {
+            PrecompilationAnalysisResult result = new PrecompilationAnalysisResult();
+
+            if (_configuration == null)
+            {
+                // In case when _configuration is not supplied, it is assumed that there are no precompilation constraints. 
+                result.AllowedVariableTypesCheckResult.IsUseOfUnallowedSyntaxElementsDetected = false;
+                result.AllowedMethodDefinitionsAndCallsCheck.IsUseOfUnallowedSyntaxElementsDetected = false;
+            }
+            else
+            {
+                // Used variable types analysis
+                result.AllowedVariableTypesCheckResult = this.PerformSyntaxElementAnalysis(this.GetUsedVariableTypes(), _configuration.AllowedVariableTypes);
+                // Used method definition or calls analysis
+                result.AllowedMethodDefinitionsAndCallsCheck = this.PerformSyntaxElementAnalysis(this.GetNamesForDefinedOrCalledMethods(), _configuration.AllowedMethodDefinitionsAndCalls);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Performs analysis of used syntax elements comparing it to allowed ones.
+        /// If list of allowed syntax elements has null value, it is assumed that there is no constraints regarding that syntax element group,
+        /// in that case it will be determined that no unallowed syntax elements where detected, and the list of used unallowed elements will be empty.
+        /// </summary>
+        /// <param name="usedSyntaxElements">Syntax elements that were used in code.</param>
+        /// <param name="allowedSyntaxElements">Allowed syntax elements.</param>
+        /// <returns>Result in form of AllowedSyntaxElementCheckResult object.</returns>
+        private AllowedSyntaxElementCheckResult PerformSyntaxElementAnalysis(List<string> usedSyntaxElements, List<string> allowedSyntaxElements)
+        {
+            AllowedSyntaxElementCheckResult result = new AllowedSyntaxElementCheckResult();
+
+            if(allowedSyntaxElements != null)
+            {
+                result = this.AreSyntaxElementsAllowed(usedSyntaxElements, allowedSyntaxElements);
+            }
+            else
+            {
+                result.IsUseOfUnallowedSyntaxElementsDetected = false;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Cheks if syntax element can be found in given list of allowed syntax elements.
+        /// </summary>
+        /// <param name="syntaxElement">Element that should be checked.</param>
+        /// <param name="allowedSyntaxElements">List of syntax elements against which syntax element check is performed.</param>
+        /// <returns>Indication if syntax element is allowed.</returns>
+        private bool IsSyntaxElementAllowed(string syntaxElement, List<string> allowedSyntaxElements)
+        {
+            bool result = false;
+
+            foreach (var allowedSyntaxElement in allowedSyntaxElements)
+            {
+                if (syntaxElement == allowedSyntaxElement)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Compares list of used syntax element against compatible list of alowed syntax elements.
+        /// </summary>
+        /// <param name="usedSyntaxElements">List of strings that represents used syntax elements.</param>
+        /// <param name="allowedSyntaxElements">List of strings that represents allowed syntax elements</param>
+        /// <returns>False if even one of used syntax elements is not in the list of allowed syntax elements, otherwise true.</returns>
+        private AllowedSyntaxElementCheckResult AreSyntaxElementsAllowed(List<string> usedSyntaxElements, List<string> allowedSyntaxElements)
+        {
+            AllowedSyntaxElementCheckResult result = new AllowedSyntaxElementCheckResult { IsUseOfUnallowedSyntaxElementsDetected = false };
+
+            foreach (string usedSyntaxElement in usedSyntaxElements)
+            {
+                if (IsSyntaxElementAllowed(usedSyntaxElement, allowedSyntaxElements) == false)
+                {
+                    result.IsUseOfUnallowedSyntaxElementsDetected = true;
+                    result.UsedUnalowedSyntaxElements.Add(usedSyntaxElement);
+                }
+            }
+
+            return result;
+        }
+    }
+
+    public class AllowedSyntaxElementCheckResult
+    {
+        public bool IsUseOfUnallowedSyntaxElementsDetected { get; set; } = true;
+        public List<string> UsedUnalowedSyntaxElements { get; set; } = new List<string>();
+    }
+
+    public class PrecompilationAnalysisResult
+    {
+        public AllowedSyntaxElementCheckResult AllowedVariableTypesCheckResult { get; set; }
+        public AllowedSyntaxElementCheckResult AllowedMethodDefinitionsAndCallsCheck { get; set; }
     }
 }
